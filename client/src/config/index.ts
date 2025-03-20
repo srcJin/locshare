d241f3d1db1bcbd7a594cf3bb84cd88c56980b43
Jin Gao <gao-jin@outlook.com>
2025-03-20 09:57:38 -0400

    update socket url

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,3 +1,7 @@
-export const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5888' : 'https://evacuation-server.onrender.com'
+// export const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5888' : 'https://evacuation-server.onrender.com'
+
+
 
 // export const SOCKET_URL = 'http://localhost:5888'
+
+export const SOCKET_URL = 'https://evacuation-server.onrender.com'
