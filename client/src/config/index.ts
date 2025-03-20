1b04a46b2d0ff3d909e66bada37aa1ecce1738e1
Jin Gao <gao-jin@outlook.com>
2025-03-20 09:17:49 -0400

    update server url

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,3 +1,3 @@
-// export const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5888' : 'https://locshare-xc0v.onrender.com'
+export const SOCKET_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5888' : 'https://evacuation-server.onrender.com'
 
-export const SOCKET_URL = 'http://localhost:5888'
+// export const SOCKET_URL = 'http://localhost:5888'
